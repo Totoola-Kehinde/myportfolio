5710cfa8cc3b839567afa037329fadaa0294e9c8
Totoola Kenny <40864358+Totoola-Kehinde@users.noreply.github.com>
2020-07-23 20:30:10 +0100

    wiseword form

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
 from flask import Flask, render_template, redirect, url_for
+from views.forms.wisewordform import wisewordForm
+import settings
 
 app = Flask(__name__)
+app.secret_key = settings.secret_key
 
 @app.route('/')
 def index():
+    form = wisewordForm()
 
-    return render_template("index.html")
+    return render_template("index.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- views/forms/wisewordform.py
+++ views/forms/wisewordform.py
@@ -1,2 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField
+from wtforms import StringField, TextField, SubmitField, BooleanField
+from wtforms.validators import data_required
+
+class wisewordForm(FlaskForm):
+    name = StringField("Input Your name Jigga!", [data_required()])
+    wiseword = TextField("Wise words from wise men!", [data_required()])
+    stayanonymous =  BooleanField("Stay Anonymous?")
+    submit = SubmitField("Submit!")
