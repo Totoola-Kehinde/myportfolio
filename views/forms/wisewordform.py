1331fec61e43a538aa1341bd3df6d55db0a25530
Totoola Kenny <40864358+Totoola-Kehinde@users.noreply.github.com>
2020-11-19 22:25:39 +0100

    fixed navigation on mobile and words on form

--- views/forms/wisewordform.py
+++ views/forms/wisewordform.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import data_required
 
 class wisewordForm(FlaskForm):
-    name = StringField("Input Your name Jigga!", [data_required()])
-    wiseword = TextAreaField("Wise words from wise men!", [data_required()])
+    name = StringField("Your name", [data_required()])
+    wiseword = TextAreaField("Wise words / Legendary Experience", [data_required()])
     stayanonymous =  BooleanField("Stay Anonymous?")
     submit = SubmitField("Submit!")
